e99663d3bf2406026ce1f8849f50437e23515b2d
rami-dv <rahsepe@gmial.com>
2020-12-27 17:33:57 -0300

    primer commit

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,5 +2,15 @@
 
 @section('content')
     <h1>Crear Post</h1>
+    <form method="post" action="">
+        @csrf
+        @method('PUT')
 
+        <label>titulo</label>
+        <input type="text" value="{{ old('titulo', 'titulo actual') }}">
+        <label>contenido</label>
+        <input type="text">
+        <input type="submit">
+    </form>
 @endsection
+
